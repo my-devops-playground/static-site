0364f73c9c02397d125441c1beb8e12647aa2b06
Mario Garcia <mario.ggar@gmail.com>
2022-09-14 14:26:31 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
         name   : "company-1-pre-superdope",
         script : "npm run-script build",
         log_type: "json",
-        restart: false,
+        restart: true,
         out_file: "/var/log/builder/builder.info.json",
         error_file: "/var/log/builder/builder.error.json",
         log_date_format: "YYYY-MM-DD HH:mm:ss",
